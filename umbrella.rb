56a97169c52d59a88491c7809919b52180f0a120
GVKaushik <gvkaushik11@gmail.com>
2025-04-09 01:51:56 +0000

    GMaps lat long done

--- umbrella.rb
+++ umbrella.rb
@@ -6,4 +6,14 @@ require "dotenv/load"
 puts "Will you need an umbrella today?"
 puts "Where are you?"
 location = gets.chomp
+#location = "Chicago Loop Millenium Park"
+location = location.gsub(" ","%20")
+google_maps_url = "https://maps.googleapis.com/maps/api/geocode/json" + "?address=#{location}"+"&key=#{ENV.fetch("GMAPS_KEY")}"
+maps_response_raw = HTTP.get(google_maps_url)
+maps_response_parsed = JSON.parse(maps_response_raw)
+#pp maps_response_parsed.fetch("results")[0]
+pp lat = maps_response_parsed.fetch("results")[0].fetch("geometry").fetch("location").fetch("lat")
+pp long = maps_response_parsed.fetch("results")[0].fetch("geometry").fetch("location").fetch("lng")
 
+
+#pirate_weather_url
